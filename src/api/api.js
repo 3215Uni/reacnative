7df6c65ac8f4e670ca033a63d59c8b5ff7b61ed5
Roxana Benicio <roxanayesicavictoriabenicio@gmail.com>
2023-04-22 04:49:40 -0300

    es con perfil

---
+++ src/api/api.js
@@ -0,0 +1,7 @@
+const API_VERSION_NUMBER = 1
+
+export const api = {
+  server: 'https://643d446cf0ec48ce90578ac8.mockapi.io', // completa con api server
+  apiVersion: `/api/v${API_VERSION_NUMBER}`
+}
+
